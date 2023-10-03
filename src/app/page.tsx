fe4950b124f6d71c7568cb68d093bdd7e52cc0f5
Diego Rivera <diegosebsk@gmail.com>
2023-10-02 23:12:30 -0300

    folder restructuring

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import {ThemeButton} from './components/ThemeButton'
-
-export default function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 dark:bg-stone-950 '>
-      <ThemeButton />
-    </main>
-  )
-}
