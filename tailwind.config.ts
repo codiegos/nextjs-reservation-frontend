d6596888ed8d612cdd8fd2d8713132356b9420a1
Diego Rivera <diegosebsk@gmail.com>
2023-10-18 22:11:57 -0300

    feat: add color pallete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,13 +9,21 @@ const config: Config = {
   darkMode: 'class',
   theme: {
     fontFamily: {
-      sans: ['var(--primary-font)'],
+      sans: ['var(--primary-font)', 'sans-serif'],
     },
     extend: {
       colors: {
-        primary: '#120037',
-        secondary: '#29007D',
-        tertiary: '#3A00B0',
+        'primary-50': '#e4e6ff',
+        'primary-100': '#cfd0ff',
+        'primary-200': '#a8a8ff',
+        'primary-300': '#7974ff',
+        'primary-400': '#5a3eff',
+        'primary-500': '#4b13ff',
+        'primary-600': '#4400ff',
+        'primary-700': '#4400ff',
+        'primary-800': '#3c00e4',
+        'primary-900': '#3500b0',
+        'primary-950': '#120037',
       },
     },
   },
