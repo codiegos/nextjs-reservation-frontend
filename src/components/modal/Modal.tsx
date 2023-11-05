2e511a4216c8ea879191c3c38e5425e4b28be46f
Diego Rivera <diegosebsk@gmail.com>
2023-11-04 21:21:40 -0300

    Add duration to modal transitions and change close button color.

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -16,12 +16,12 @@ function Modal({ children }: ModalProps) {
 
   return (
     <section
-      className={`fixed inset-0 z-50 grid place-items-center transition-colors lg:pl-64 ${
+      className={`fixed inset-0 z-50 grid place-items-center transition-colors duration-200 lg:pl-64 ${
         isModalOpen ? 'bg-black bg-opacity-50' : 'invisible'
       }`}
     >
       <article
-        className={`max-w-2xl rounded-lg border bg-white p-14 shadow transition-all dark:border-slate-800 dark:bg-slate-900 ${
+        className={`max-w-2xl rounded-lg border bg-white p-12 pb-8 shadow transition-all duration-200 dark:border-slate-800 dark:bg-slate-900 ${
           isModalOpen ? 'scale-100 opacity-100' : 'scale-110 opacity-0'
         }`}
         onClick={(e) => {
@@ -29,7 +29,7 @@ function Modal({ children }: ModalProps) {
         }}
       >
         <button onClick={onClose} className='absolute right-4 top-4'>
-          <XIcon className='h-8 w-8 duration-150 hover:scale-105' />
+          <XIcon className='h-8 w-8 duration-150 hover:scale-105 hover:text-red-400' />
         </button>
 
         {children}
