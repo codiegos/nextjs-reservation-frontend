dfb41fb89687be6e726f0b0bdb80bbeb908de0ba
Diego Rivera <diegosebsk@gmail.com>
2023-11-04 21:21:10 -0300

    Update WarningAlert component styling and text

--- src/components/alert/WarningAlert.tsx
+++ src/components/alert/WarningAlert.tsx
@@ -3,13 +3,12 @@ import { InfoIcon } from '../icons'
 function WarningAlert() {
   return (
     <aside
-      className='dark:bg-primary-900 mb-4 flex items-center gap-4 rounded-lg bg-yellow-50 p-4 text-sm text-yellow-800 dark:text-yellow-300'
+      className='mb-4 flex items-center gap-4 rounded-lg bg-yellow-50 p-4 text-sm text-yellow-800 dark:bg-primary-900 dark:text-yellow-300'
       role='alert'
     >
       <InfoIcon className='h-6 w-6 flex-none text-yellow-400' />
       <p>
-        <span className='font-medium'>Warning alert!</span> Change a few things
-        up and try submitting again.
+        <span className='font-medium'>Alerta!</span> No se han encontrado datos.
       </p>
     </aside>
   )
