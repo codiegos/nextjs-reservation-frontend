e1997ef4a7bce27b99b259e6f954c97fe69ca93b
Diego Rivera <diegosebsk@gmail.com>
2023-11-10 20:43:49 -0300

    Add new button components to index.tsx

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,4 +1,13 @@
 import PrimaryButton from './PrimaryButton'
 import ThemeButton from './ThemeButton'
+import DangerButton from './DangerButton'
+import OutlineButton from './OutlineButton'
+import PaginationButton from './PaginationButton'
 
-export { PrimaryButton, ThemeButton }
+export {
+  PrimaryButton,
+  ThemeButton,
+  DangerButton,
+  OutlineButton,
+  PaginationButton,
+}
