54f018c63c08775fb15245cd49a81015ccfa8ccd
Diego Rivera <diegosebsk@gmail.com>
2023-11-18 23:20:22 -0300

    Refactor dashboard layout and add sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,22 @@
-import Navbar from '@/components/Navbar'
-import { ThemeButton } from '@/components/ThemeButton'
+import { Sidebar } from '@/components/sidebar'
+import { ThemeButton } from '@/components/button'
+import { ModalProvider } from '@/contexts/modal-context'
 interface DashboardLayoutProps {
   children: React.ReactNode
 }
 function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <div className='grid min-h-screen lg:grid-cols-4 xl:grid-cols-6'>
-      <Navbar />
-      <main className='h-[100vh] p-16 lg:col-span-3 lg:px-20 xl:col-span-5'>
-        {children}
-      </main>
-      <ThemeButton className='fixed right-4 top-[3.8rem] rounded-md p-2 duration-200 hover:bg-pink-200 dark:hover:bg-pink-800 lg:right-12' />
-    </div>
+    <ModalProvider>
+      <div className='grid lg:grid-cols-12'>
+        <Sidebar />
+        <main className='m-8 rounded-3xl bg-white px-8 shadow dark:bg-slate-900 dark:shadow-slate-800 lg:col-span-9 2xl:col-span-10'>
+          <span className='flex justify-end py-8 pb-16 lg:-mr-4'>
+            <ThemeButton className='fixed rounded-md p-2 duration-200 hover:bg-primary-50 dark:hover:bg-primary-900' />
+          </span>
+          {children}
+        </main>
+      </div>
+    </ModalProvider>
   )
 }
 export default DashboardLayout
