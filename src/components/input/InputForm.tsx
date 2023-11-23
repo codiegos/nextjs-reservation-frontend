c345208da22f4da8c5f4202cca1a6a0aa0a6c5eb
Diego Rivera <diegosebsk@gmail.com>
2023-11-22 23:46:23 -0300

    Refactor InputForm component to include optional className and icon props

--- src/components/input/InputForm.tsx
+++ src/components/input/InputForm.tsx
@@ -1,20 +1,30 @@
 import { InputHTMLAttributes } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: string
+  label?: string
   htmlFor: string
   error?: string
   register?: any
+  className?: string
+  icon?: React.ReactNode
 }
 
-function InputForm({ label, htmlFor, register, error, ...props }: InputProps) {
+function InputForm({
+  label,
+  htmlFor,
+  register,
+  error,
+  className,
+  icon: Icon,
+  ...props
+}: InputProps) {
   return (
-    <div className='grid gap-1.5'>
-      <label htmlFor={htmlFor}>{label}:</label>
+    <div className={'relative grid gap-1.5'}>
+      {label && <label htmlFor={htmlFor}>{label}:</label>}
       <input
         {...register}
         id={htmlFor}
-        className={`rounded-md p-2.5 pr-4 placeholder-slate-400/90 outline-none ring-1 focus:ring-[1.5px] dark:bg-primary-950/50 ${
+        className={`relative p-2.5 pr-4 placeholder-slate-400/90 outline-none ring-1 focus:ring-[1.5px] dark:bg-primary-950/50 ${className} rounded-md ${
           error
             ? 'ring-red-500 focus:ring-red-500'
             : 'ring-primary-900 focus:ring-primary-500'
@@ -22,6 +32,7 @@ function InputForm({ label, htmlFor, register, error, ...props }: InputProps) {
         {...props}
       />
       {<span className='h-3 text-sm text-red-500 2xl:text-base'>{error}</span>}
+      {Icon && <span className='absolute inset-y-1/4 right-4'>{Icon}</span>}
     </div>
   )
 }
